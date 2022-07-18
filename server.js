25501e20291ce65bd88a4f82877c31a3c356436c
jabbar-dev <rashdijabbar@gmail.com>
2022-07-18 17:04:06 +0500

    CORS Config

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const dotenv = require('dotenv');
 const auth = require('./routes/api/auth');
 require('dotenv').config();
 
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 //Connect to MongoDB ATLAS
 mongoose.connect(process.env.MONGO_URL,
     {useNewUrlParser:true}
